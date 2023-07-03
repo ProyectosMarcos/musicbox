be15d7d1f847fa56d895263e7f1b6c4de59ba472
gaboalvarez <alvarezgabriel907@gmail.com>
2023-07-03 03:52:44 -0300

    correcto renderizado de INICIO

--- musicboxWebApp/views.py
+++ musicboxWebApp/views.py
@@ -4,6 +4,16 @@ from django.template import Template,Context
 from django.template.loader import get_template
 from django.shortcuts import render
 
+
 def inicio(request):
-    nombre_usuario = "gabo_alvarez"
-    return render(request,"musicboxWebApp/INICIO.html",{"nombreUsuario":nombre_usuario})
+    novedades = ["Linkin Park - Hybrid Theory","Linkin Park - Meteora","Linkin Park - Minutes to Midnight","Limp Bizkit - Chocolate Starfish And The Hot Dog Flavored Water"]
+    dto_inicio = {
+	    "nombreUsuario":"gabo_alvarez",
+	    "cantListas":1,
+	    "cantAlbumesListenlist":2,
+	    "cantUsuariosSeguidos":3,
+	    "cantUsuariosSeguidores":4,
+	    "novedades":novedades
+    }
+
+    return render(request,"musicboxWebApp/INICIO.html",dto_inicio)
