3e96754a212bcf1f3b52ee123674132cd245cf02
gaboalvarez <alvarezgabriel907@gmail.com>
2023-07-03 02:19:24 -0300

    ruteo INICIO

---
+++ musicbox/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.template import Template,Context
+from django.template.loader import get_template
+from django.shortcuts import render
+
+def inicio(request):
+    nombre_usuario = "gabo_alvarez"
+    return render(request,"INICIO.html",{"nombreUsuario":nombre_usuario})
