df405a72bc3739c6dccf68de28213b12397c43b1
gaboalvarez <alvarezgabriel907@gmail.com>
2023-07-04 20:33:57 -0300

    Base para crear modelos (BD)

--- musicbox/settings.py
+++ musicbox/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'musicboxWebApp',
+    'musicboxApp',
+    'musicboxWebApp'
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'musicbox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'musicbox',
+        'USER': 'postgres',
+        'PASSWORD': '123454321',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
