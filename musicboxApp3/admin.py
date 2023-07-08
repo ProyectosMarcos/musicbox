ff41ab4f9631a9e3af7d7ee46f9af9ade8d3de40
gaboalvarez <alvarezgabriel907@gmail.com>
2023-07-08 03:31:11 -0300

    modelo para BD

--- musicboxApp3/admin.py
+++ musicboxApp3/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from musicboxApp3.models import Usuario,Album,Cancion,Lista,Albums_x_Lista 
 
-# Register your models here.
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display=("usuario","contrasenia","email","archivo_imagen") 
+
+class AlbumAdmin(admin.ModelAdmin):
+    list_display=("artista","nombre","link_referencia","archivo_imagen")  
+
+class CancionAdmin(admin.ModelAdmin):
+    list_display=("album","nombre")
+
+class ListaAdmin(admin.ModelAdmin):
+    list_display=("usuario","nombre","descripcion","archivo_imagen")  
+
+class Albums_x_ListaAdmin(admin.ModelAdmin):
+    list_display=("lista","album")
+
+
+admin.site.register(Usuario,UsuarioAdmin)
+admin.site.register(Album,AlbumAdmin)
+admin.site.register(Cancion,CancionAdmin)
+admin.site.register(Lista,ListaAdmin)
+admin.site.register(Albums_x_Lista,Albums_x_ListaAdmin)
